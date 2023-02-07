c3fe5ba9b088db9d704bc37af413f80db3d112e0
Blck Strt <97315523+BlockStrt@users.noreply.github.com>
2023-02-07 14:17:02 -0600

    update

--- pages/store.js
+++ pages/store.js
@@ -10,17 +10,17 @@ import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 
 const slides = [
-    { image: 'https://cdn.sanity.io/images/7fnjll7q/production/0a42dc30669eeb70ba6785605690b218c2acf923-1024x1024.jpg',
+    { image: 'https://cdn.sanity.io/images/cpwmjnrl/production/03413995552f59fbc3e8d98517442fe2db87eed7-3024x4032.jpg',
      title: 'Product 1' ,
      price: '$125',
      description: 'First Product description here '
     },
-    { image: 'https://cdn.sanity.io/images/7fnjll7q/production/6ede803103f3d9791389a347f9b430ae44ba1e61-1024x1024.jpg',
+    { image: 'https://cdn.sanity.io/images/cpwmjnrl/production/4bef8db8bcc0e504998833c604ccc8a4417e1a79-3024x4032.jpg',
      title: 'Product 2' ,
      price: '$225',
      description: 'Second Product description here '
     },
-    { image: 'https://cdn.sanity.io/images/7fnjll7q/production/0a42dc30669eeb70ba6785605690b218c2acf923-1024x1024.jpg', 
+    { image: 'https://cdn.sanity.io/images/cpwmjnrl/production/ba6692d89a30ed33f0b84b9e9598e97de6082645-3024x4032.jpg', 
     title: 'Product 3' ,
      price: '$325',
      description: 'Third Product description here '
@@ -62,7 +62,7 @@ export default function Store(){
             {slides.map((slide, index) => (
                 <div key={index} className={`relative flex inset-0 z-10 ${index === currentSlide ? 'block' : 'hidden'}`}>
                
-                <img src={slide.image} alt="" className="object-cover p-10 " />
+                <img src={slide.image} alt="" className="object-cover p-10 h-[100vh] " />
                 
                 <div className=" top-40 left-10 w-full justify-center items-center mx-auto flex ">
                 <button className='text-3xl ' onClick={handlePreviousClick}><ArrowBackIosIcon/></button>
@@ -76,7 +76,7 @@ export default function Store(){
     
     </div>
             {/* checkout container */}
-        <div className=' w-[50%] h-[40%] grid items-center justify-center mx-auto mb-28 '>
+        <div className=' w-[50%] h-[50%] grid items-center justify-center mx-auto mb-28 '>
             {/* Title pricing container */}
             <div className=' '>
              <ul className=''>
