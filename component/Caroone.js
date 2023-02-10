9b6247bc1cd903e156f5011656cc017b2987dda2
Blck Strt <97315523+BlockStrt@users.noreply.github.com>
2023-02-10 14:41:42 -0600

    update

--- component/Caroone.js
+++ component/Caroone.js
@@ -36,7 +36,7 @@ const slides = [
     return(
         <div className=" w-[100%] md:w-[100%] ">
         {slides.map((slide, index) => (
-            <div key={index} className={`relative flex inset-0 z-10 justify-center items-center ${index === currentSlide ? 'block' : 'hidden'}`}>
+            <div key={index} className={` ${index === currentSlide ? 'block' : 'hidden'}`}>
         
             <img src={slide.image} alt="" className="object-cover p-10 w-full md:h-[100vh] " />
             
