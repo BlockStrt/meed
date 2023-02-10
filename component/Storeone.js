43c9c122988f7c049cc17cf682648d7d4785c3a9
Blck Strt <97315523+BlockStrt@users.noreply.github.com>
2023-02-10 16:41:20 -0600

    update

--- component/Storeone.js
+++ component/Storeone.js
@@ -4,7 +4,6 @@ import  ReactSelect from 'react-select';
 import ButtonResult from '@/component/ButtonResult';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import Caroone from './Caroone';
 
 
 
@@ -38,6 +37,7 @@ const slides = [
     const { handleSubmit, reset, setValue, control } = useForm({ defaultValues });
     const [data, setData] = useState(null);
     const onSubmit = data => console.log(data);
+
     const handlePreviousClick = () => {
         setCurrentSlide(currentSlide === 0 ? slides.length - 1 : currentSlide - 1);
         setCurrentProduct(slides[currentSlide]);
@@ -46,93 +46,87 @@ const slides = [
         setCurrentSlide(currentSlide === slides.length - 1 ? 0 : currentSlide + 1);
         setCurrentProduct(slides[currentSlide]);
     }
-
     return(
 <div className=' md:flex  p-2 h-full  '>
-    
-    {/* image container */}
-    
-<div className=" w-[100%] md:w-[100%] ">
-      {slides.map((slide, index) => (
-          <div key={index} className={`relative flex inset-0 z-10 ${index === currentSlide ? 'block' : 'hidden'}`}>
-         
-          <img src={slide.image} alt="" className="object-cover p-10 h-[100%] md:h-[100vh] " />
-          
-          <div className=" top-40 left-10 justify-center items-center mx-auto flex ">
-          <button className='text-3xl ' onClick={handlePreviousClick}><ArrowBackIosIcon/></button>
-          <button className='text-3xl ' onClick={handleNextClick}><ArrowForwardIosIcon /></button>
+          {/* image container */}          
+    <div className=" w-[100%] md:w-[100%] ">
+            {slides.map((slide, index) => (
+                <div key={index} className={`relative flex inset-0 z-10 ${index === currentSlide ? 'block' : 'hidden'}`}>
+               
+                <img src={slide.image} alt="" className="object-cover p-10 h-[100%] md:h-[100vh] " />
+                
+                <div className=" top-40 left-10 justify-center items-center mx-auto flex ">
+                <button className='text-3xl ' onClick={handlePreviousClick}><ArrowBackIosIcon/></button>
+                <button className='text-3xl ' onClick={handleNextClick}><ArrowForwardIosIcon /></button>
+                </div>
+                
           </div>
           
+      ))}
     </div>
-    
-))}
+            {/* checkout container */}
+        <div className=' w-[100%] h-[50%] grid items-center  justify-center mx-auto mb-28 '>
+            {/* Title pricing container */}
+            <div className=' '>
+             <ul className=''>
+                <li>
+                <h1 className='text-2xl'>{currentProduct.title}</h1>
+                </li>
+                <li className='m-1'>
+                    <p>
+                        <strong>{currentProduct.price}</strong>
+                    </p>
+                </li>
+             </ul>
+             {/* shippig info  */}
+                <div>
+                    <h2 className='m-1'>Details</h2>
+                    <div className='w-full border border-black opacity-10'></div>
+                </div>
+                <div>
+                    <p className='mt-4 uppercase mb-4'>please alow 1-2 days for item to ship after payments</p>
+                </div>
 
-
-</div>
-      {/* checkout container */}
-  <div className=' w-[100%] h-[50%] grid items-center  justify-center mx-auto mb-28 '>
-      {/* Title pricing container */}
-      <div className=' '>
-       <ul className=''>
-          <li>
-          <h1 className='text-2xl'>{currentProduct.title}</h1>
-          </li>
-          <li className='m-1'>
-              <p>
-                  <strong>{currentProduct.price}</strong>
-              </p>
-          </li>
-       </ul>
-       {/* shippig info  */}
-          <div>
-              <h2 className='m-1'>Details</h2>
-              <div className='w-full border border-black opacity-10'></div>
-          </div>
-          <div>
-              <p className='mt-4 uppercase mb-4'>please alow 1-2 days for item to ship after payments</p>
-          </div>
-
-          <form onSubmit={handleSubmit(onSubmit)}>
-          <label className='p-2 '>Quantity</label>
-          <div className='w-full border border-black mb-2 mt-1 opacity-10'></div>
-              <Controller
-                  name="Qty"
-                  control={control}
-                  render={({ field }) => (
-                  <ReactSelect
-                      isClearable
-                      {...field}
-                      options={[
-                      { value: "1", label: "1" },
-                      { value: "2", label: "2" },
-                      { value: "3", label: "3" },
-                      ]}
-                  />
-                  )}
-              />
-          <label className='p-2'>Coming soon</label>
-          <div className='w-full border border-black mb-2 mt-1 opacity-10'></div>
-              <Controller
-                  name="Coming"
-                  control={control}
-                  render={({ field }) => (
-                  <ReactSelect
-                      isClearable
-                      {...field}
-                      options={[
-                      { value: "Coming soon", label: "Coming soon" },
-                      { value: "Coming soon", label: "Coming soon" },
-                      { value: "Coming soon", label: "Coming soon" }
-                      ]}
-                  />
-                  )}
-              />
-              <ButtonResult {...{ data, reset, setValue }} />
-          </form>
-      
-      </div>
-  </div>
-  
+                <form onSubmit={handleSubmit(onSubmit)}>
+                <label className='p-2 '>Quantity</label>
+                <div className='w-full border border-black mb-2 mt-1 opacity-10'></div>
+                    <Controller
+                        name="Qty"
+                        control={control}
+                        render={({ field }) => (
+                        <ReactSelect
+                            isClearable
+                            {...field}
+                            options={[
+                            { value: "1", label: "1" },
+                            { value: "2", label: "2" },
+                            { value: "3", label: "3" },
+                            ]}
+                        />
+                        )}
+                    />
+                <label className='p-2'>Coming soon</label>
+                <div className='w-full border border-black mb-2 mt-1 opacity-10'></div>
+                    <Controller
+                        name="Coming"
+                        control={control}
+                        render={({ field }) => (
+                        <ReactSelect
+                            isClearable
+                            {...field}
+                            options={[
+                            { value: "Coming soon", label: "Coming soon" },
+                            { value: "Coming soon", label: "Coming soon" },
+                            { value: "Coming soon", label: "Coming soon" }
+                            ]}
+                        />
+                        )}
+                    />
+                    <ButtonResult {...{ data, reset, setValue }} />
+                </form>
+            
+            </div>
+        </div>       
 </div>
     )
   }
